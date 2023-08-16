b72c90611d3fbdf4b91b56f0437ad448cee06851
1adept <69433209+1adept@users.noreply.github.com>
2023-08-16 19:57:09 +0200

    Rust day02 start

--- rust/src/day02.rs
+++ rust/src/day02.rs
@@ -1,38 +1,116 @@
-// use crate::Day;
-// use regex::Regex;
+use crate::Day;
+use lazy_static::lazy_static;
+use regex::Regex;
 
-// const EXAMPLE: &'static str = include_str!("../../data/02_example.in");
+lazy_static! {
+    static ref PWD_REGEX: Regex =
+        Regex::new(r"(?<min>\d+)-(?<max>\d+) (?<char>[a-z]): (?<pwd>\w+)").unwrap();
+}
 
-// struct Day02 {}
-// impl Day for Day02 {
-//     fn solve1(input: String) -> u32 {
-//         input
-//             .lines()
-//             .flat_map(|line| {
-//                 if let Some(captures) = Day02::PWD_REGEX.captures(line) {
-//                     let min = captures.name("min");
-//                     let max = captures.name("max");
-//                     let chars = captures.name("char");
-//                     let pwd = captures.name("pwd");
+pub struct Day02 {
+    passwords: Vec<Password>,
+}
+struct Password {
+    min: usize,
+    max: usize,
+    character: char,
+    password: String,
+}
 
-//                     let occurances = pwd.iter().filter(|c| c == chars).count();
-//                     if occurances >= min && occurances <= max {
-//                         None
-//                     } else {
-//                         Some(pwd)
-//                     }
-//                 }
-//             })
-//             .count()
-//     }
+impl Day for Day02 {
+    fn parse(text: &str) -> Self {
+        let data = text
+            .lines()
+            .flat_map(|line| {
+                if let Some(captures) = PWD_REGEX.captures(line) {
+                    let min = captures
+                        .name("min")
+                        .map(|x| x.as_str())
+                        .map(|s| s.parse())
+                        .unwrap()
+                        .unwrap();
+                    let max = captures
+                        .name("max")
+                        .map(|x| x.as_str())
+                        .map(|s| s.parse())
+                        .unwrap()
+                        .unwrap();
+                    let character: char = captures
+                        .name("char")
+                        .unwrap()
+                        .as_str()
+                        .chars()
+                        .next()
+                        .unwrap();
+                    let password = captures.name("pwd").unwrap().as_str().to_owned();
 
-//     fn solve2(input: String) {
-//         todo!()
-//     }
-// }
-// impl Day02 {
-//     const PWD_REGEX: Regex =
-//         Regex::new(r"(?<min>\d+)-(?<max>\d+) (?<char>[a-z]): (?<pwd>\w+)").unwrap();
+                    Some(Password {
+                        min,
+                        max,
+                        character,
+                        password,
+                    })
+                } else {
+                    None
+                }
+            })
+            .collect();
+        Self { passwords: data }
+    }
 
-//     fn parse(input: &str) -> &[&str] {}
-// }
+    fn solve1(&self) -> usize {
+        self.passwords
+            .iter()
+            .filter(
+                |Password {
+                     min,
+                     max,
+                     character,
+                     password,
+                 }| {
+                    let occurances = &password.chars().filter(|c| c == character).count();
+                    occurances >= min && occurances <= max
+                },
+            )
+            .count()
+    }
+
+    fn solve2(&self) -> usize {
+        self.passwords
+            .iter()
+            .filter(
+                |Password {
+                     min,
+                     max,
+                     character,
+                     password,
+                 }| {
+                    assert_ne!(0, *min);
+                    assert_ne!(0, *max);
+                    let mut window = password.as_str()[*min..*max].chars();
+                    let first = window.next().map(|c| c == *character).unwrap_or(false);
+                    first || window.last().map(|c| c == *character).unwrap_or(false)
+                },
+            )
+            .count()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{day02::Day02, Day};
+
+    pub const EXAMPLE: &'static str = include_str!("../../data/02_example.in");
+
+    #[test]
+    fn test_part11() {
+        let day = Day02::parse(EXAMPLE);
+        assert_eq!(2, day.solve1())
+    }
+
+    #[test]
+    fn test_part2() {
+        let day = Day02::parse(EXAMPLE);
+        assert_eq!(1, day.solve2())
+    }
+}
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,11 +1,36 @@
 mod day02;
 
+use std::{env, process};
+
 trait Day {
-    fn solve1(input: String);
-    fn solve2(input: String);
+    fn parse(text: &str) -> Self;
+    fn solve1(&self) -> usize;
+    fn solve2(&self) -> usize;
 }
 
 fn main() {
-    // let day02 = day02::new();
-    println!("Hello world");
+    let mut args = env::args();
+    args.next().unwrap();
+
+    if let (Some(day_number), Some(path)) = (args.next(), args.next()) {
+        println!("Got day {day_number} and file {path}");
+
+        let day_number = day_number
+            .parse()
+            .expect("Please provide a valid day-number");
+        if let Ok(text) = std::fs::read_to_string(path) {
+            let day = match day_number {
+                2 => day02::Day02::parse(&text),
+                _ if day_number > 25 => unreachable!("Too high"),
+                _ if day_number < 0 => unreachable!("Too low"),
+                _ => todo!("Day not done yet!"),
+            };
+
+            println!("Day {day_number:0>2} Part1 => {}", day.solve1());
+            println!("Day {day_number:0>2} Part2 => {}", day.solve2());
+        }
+    } else {
+        eprintln!("Please provide day and input data");
+        process::exit(1);
+    }
 }
