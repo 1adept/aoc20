675e402d06f1f7ca9d8003a9193692b9f5fc0580
1adept <69433209+1adept@users.noreply.github.com>
2023-08-16 19:58:22 +0200

    Trash

--- rust/target/debug/deps/rust-7a840e899ea99753.d
+++
@@ -1,6 +0,0 @@
-/home/adept/code/aoc-20/rust/target/debug/deps/rust-7a840e899ea99753: src/main.rs src/day02.rs
-
-/home/adept/code/aoc-20/rust/target/debug/deps/rust-7a840e899ea99753.d: src/main.rs src/day02.rs
-
-src/main.rs:
-src/day02.rs:
