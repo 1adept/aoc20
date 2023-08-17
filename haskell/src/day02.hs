6c64cef47ffc2f8ad8cd278f0b4721132708f5e5
1adept <69433209+1adept@users.noreply.github.com>
2023-08-17 19:11:18 +0200

    Haskell day 02 start

--- haskell/src/day02.hs
+++ haskell/src/day02.hs
@@ -1,6 +1,20 @@
+import Data.Text (Text)
+import Data.Maybe
 
-import Text.Megaparsec
+data Password = Password {
+    min :: Int,
+    max :: Int,
+    char :: Char,
+    pwd :: String
+}
 
 main :: IO ()
 main = do
     putStrLn "End of day 02"
+
+-- 1-3 a: abcdef
+parse :: Text -> [Password]
+parse txt = 
+  let 
+    parseNum
+  in undefined
