2de35bd69486bb8b95f9c82d054f85b07cbaff48
1adept <69433209+1adept@users.noreply.github.com>
2023-08-17 21:36:16 +0200

    Haskell day 02

--- haskell/src/day02.hs
+++ haskell/src/day02.hs
@@ -1,20 +1,65 @@
-import Data.Text (Text)
-import Data.Maybe
+{-# OPTIONS_GHC -Wno-name-shadowing #-}
+
+import qualified Data.Text as T
+import qualified Data.Text.IO as T
+import Text.Printf (printf)
 
 data Password = Password {
-    min :: Int,
-    max :: Int,
-    char :: Char,
-    pwd :: String
-}
+  low :: Int,
+  high :: Int,
+  character :: Char,
+  pwd :: String
+} deriving Show
 
 main :: IO ()
 main = do
+    example <- T.readFile "../data/02_example.in"
+    input <- T.readFile "../data/02_input.in"
+
+    let ePwd = parse example
+    let iPwd = parse input
+
+    printf "Example part1: %s\n" $ show (solve1 ePwd)
+    printf "Example part2: %s\n" $ show (solve2 ePwd)
+
+    printf "Input part1 %s\n" $ show (solve1 iPwd)  
+    printf "Input part2 %s\n" $ show (solve2 iPwd)
+
     putStrLn "End of day 02"
 
--- 1-3 a: abcdef
-parse :: Text -> [Password]
-parse txt = 
+solve1, solve2 :: [Password] -> Int
+solve1 =
   let 
-    parseNum
-  in undefined
+    isValid p =
+      let
+        c = character p
+        charCount = length . filter (== c) $ pwd p
+      in (charCount >= low p) && (charCount <= high p)
+  in length . filter isValid
+
+solve2 =
+  let
+    isValid p =
+      let
+        first = pwd p !! (low p - 1)
+        last = pwd p !! (high p - 1)
+        c = character p
+      in (first == c) /= (last == c)
+  in length . filter isValid
+
+parse :: T.Text -> [Password]
+parse txt =
+    let
+      parse' :: T.Text -> Password
+      parse' t =
+        let
+          words = map T.unpack $ T.words t
+          minmax = head words
+          -- "Cannot find Data.List.Split (splitOn)"
+          -- lh = splitOn "-" minmax
+          low = read . takeWhile (/= '-') $ minmax
+          high = read . drop 1 . dropWhile (/= '-') $ minmax
+          c   = head $ words !! 1
+          pwd = words !! 2
+        in Password low high c pwd
+    in map parse' $ T.lines txt
